68620ea7963cf7fbea8502c1e7af549d5997efbf
ArturoAyala99 <ayala_ayax@hotmail.com>
2025-03-01 16:54:07 -0500

    third commmit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -26,3 +26,27 @@ def get_request(endpoint, **kwargs):
     except:
         # If any error occurs
         print("Network exception occurred")
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
+
+'''
+take a data dictionary in and call the add_review in the backend. 
+The dictionary would take all the values required for the dealership review as key-value, pair.
+'''
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
