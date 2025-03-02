d8dc902cbdb9f16edf3f3b11b3137269fcac4004
ArturoAyala99 <60392725+ArturoAyala99@users.noreply.github.com>
2025-03-02 14:20:41 -0600

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, 
+                                           primary_key=True, serialize=False,
+                                           verbose_name='ID')),
                 ('Name', models.CharField(max_length=100)),
                 ('Description', models.TextField()),
                 ('Color', models.CharField(max_length=100)),
@@ -27,9 +29,15 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('Name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')], default='SUV', max_length=10)),
-                ('year', models.IntegerField(default=2023, validators=[django.core.validators.MaxValueValidator(2023), django.core.validators.MinValueValidator(2015)])),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                ('type', models.CharField(choices=[
+                    ('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')
+                ], default='SUV', max_length=10)),
+                ('year', models.IntegerField(default=2023, 
+                                             validators=[django.core.validators.MaxValueValidator(2023), 
+                                                         django.core.validators.MinValueValidator(2015)])),
+                ('car_make', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='djangoapp.carmake')),
             ],
         ),
     ]
