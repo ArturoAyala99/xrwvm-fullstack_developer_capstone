8a11d07fec637910c2bc2c3b1775393b64905bd1
ArturoAyala99 <60392725+ArturoAyala99@users.noreply.github.com>
2025-03-02 15:16:32 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake (models.Model):
     Name = models.CharField(max_length=100)
@@ -10,24 +10,25 @@ class CarMake (models.Model):
     def __str__ (self): # method to print a car make object
         return self.Name
 
+
 class CarModel (models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) # Many-to-one relationship to CarMake model 
+    car_make = models.ForeignKey(CarMake, on_delete = models.CASCADE) # Many-to-one relationship to CarMake model 
     # (One car make can have many car models, using a ForeignKey field)
     # Dealer_Id = models.IntegerField(default=0)
-    Name = models.CharField(max_length=100)
+    Name = models.CharField(max_length = 100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    type = models.CharField(max_length = 10, choices = CAR_TYPES, default = 'SUV')
+    year = models.IntegerField(default = 2023,
         validators = [
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
 
+    
     def __str__(self):
         return self.Name
-        
